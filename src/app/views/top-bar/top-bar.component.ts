b68723cb398601bcf5af40892a3c1534e31218d2
Kieran Mueller (OSV) <kieran.mueller.osv@fedex.com>
2024-05-22 19:40:33 -0500

    stuff

--- src/app/views/top-bar/top-bar.component.ts
+++ src/app/views/top-bar/top-bar.component.ts
@@ -1,5 +1,6 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-top-bar',
@@ -9,9 +10,9 @@ import { Component } from '@angular/core';
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
-  status = false;
+  status = new BehaviorSubject<boolean>(false)
 
   toggle() {
-    this.status = !this.status
+    this.status.next(!this.status.value)
   }
 }
