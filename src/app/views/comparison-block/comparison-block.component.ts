2206423074b2d1b9357b29ade2ed5659d5ba6a12
Kieran Mueller (OSV) <kieran.mueller.osv@fedex.com>
2024-05-23 21:08:10 -0500

    just about done

--- src/app/views/comparison-block/comparison-block.component.ts
+++ src/app/views/comparison-block/comparison-block.component.ts
@@ -1,41 +1,65 @@
-import { NgClass, NgFor } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SharedService } from '../../service/shared.service';
 
 @Component({
   selector: 'app-comparison-block',
   standalone: true,
-  imports: [NgFor, FormsModule, NgClass],
+  imports: [NgFor, FormsModule, NgClass, NgIf],
   templateUrl: './comparison-block.component.html',
   styleUrl: './comparison-block.component.scss'
 })
-export class ComparisonBlockComponent {
+export class ComparisonBlockComponent implements OnInit {
   words = [
-    {word: '', copied: false},
-    {word: '', copied: false}
+    {word: '', copied: false, equalsBase: false},
+    {word: '', copied: false, equalsBase: false}
   ]
   allEqual = false;
+  isCaseSensitive = false
 
-  @HostListener('keyup', ['$event'])
+  constructor(private service: SharedService) {}
+
+  ngOnInit() {
+    this.service.isCaseSensitive.subscribe({
+      next: val => this.isCaseSensitive = val
+    })
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  @HostListener('window:click', ['$event'])
   onEvent($event: any) {
-    this.allEqual = this.words.every(el => {
-      return el.word.trim() === this.words[0].word.trim() ||
-             (el.word === '' && this.words.length > 2)
+    if ($event.target.classList[0] === 'word-tag') return
+    this.words.forEach(obj => {
+      if (this.isCaseSensitive) {
+        if (obj.word.trim() === this.words[0].word.trim()) {
+          obj.equalsBase = true
+        } else obj.equalsBase = false
+      } else {
+        if (obj.word.trim().toLowerCase() === this.words[0].word.trim().toLowerCase()) {
+          obj.equalsBase = true
+        } else obj.equalsBase = false
+      }
     })
   }
 
   addInput() {
-    this.words.push({word: '', copied: false})
+    this.words.push({word: '', copied: false, equalsBase: false})
+  }
+
+  deleteInput(index: number) {
+    this.words.splice(index, 1)
   }
 
   copy(index: number) {
     this.words[index].copied = true
+    navigator.clipboard.writeText(this.words[index].word)
     setTimeout(() => {
       this.words[index].copied = false
-    }, 1000)
+    }, 800)
   }
 
   test() {
-    console.log(this.words)
+    console.log(this.isCaseSensitive)
   }
 }
--- src/app/views/top-bar/top-bar.component.ts
+++ src/app/views/top-bar/top-bar.component.ts
@@ -1,6 +1,7 @@
-import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { DOCUMENT, NgClass } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { SharedService } from '../../service/shared.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
@@ -9,10 +10,55 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss'
 })
-export class TopBarComponent {
-  status = new BehaviorSubject<boolean>(false)
+export class TopBarComponent implements OnInit {
+  status = false
+  aboutBtnText = 'about'
+
+  constructor(private service: SharedService, @Inject(DOCUMENT) private document: Document, private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit() {
+    const localStorage = this.document.defaultView?.localStorage
+    if (localStorage) {
+      const localIsCaseSensitive = JSON.parse(localStorage.getItem('case-sensitive')!)
+      if (localIsCaseSensitive) {
+        this.status = localIsCaseSensitive
+        this.service.isCaseSensitive.next(localIsCaseSensitive)
+      } else {
+        this.status = this.service.isCaseSensitive.value
+      }
+    }
+    switch (this.router.url) {
+      case '/home': {
+        this.aboutBtnText = 'about'
+        break;
+      }
+      case '/about': {
+        this.aboutBtnText = 'back home'
+        break;
+      }
+    }
+  }
 
   toggle() {
-    this.status.next(!this.status.value)
+    this.status = !this.status
+    this.service.isCaseSensitive.next(this.status)
+    this.updateLocalStorage()
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('case-sensitive', JSON.stringify(this.status))
+  }
+
+  routeBetweenAboutAndHome() {
+    switch (this.router.url) {
+      case '/home': {
+        this.router.navigateByUrl('/about')
+        break;
+      }
+      case '/about': {
+        this.router.navigateByUrl('/home')
+        break;
+      }
+    }
   }
 }
